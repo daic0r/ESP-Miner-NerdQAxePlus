17b0e86eb542b3817c5ec92c1f9c56d432de52d5
daic0r <daic0r.dev@gmail.com>
2025-07-07 18:43:24 +0200

    Wrongly named variable

--- components/bm1397/include/asic.h
+++ components/bm1397/include/asic.h
@@ -77,7 +77,7 @@ class Asic {
 protected:
     float m_current_frequency;
     uint8_t m_asic_count{};
-    std::vector<float> m_vAsicFrequencies{};
+    std::vector<float> m_vCurrentFrequencies{};
 
     void send(uint8_t header, uint8_t *data, uint8_t data_len, bool debug);
     void send2(uint8_t header, uint8_t b0, uint8_t b1);
